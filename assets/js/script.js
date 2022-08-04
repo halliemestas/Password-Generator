bac417b740168cc385313f84a403c333032dbea7
Hallie <halliemestas@gmail.com>
2022-08-04 13:17:27 -0600

    updates to ensure proper execution for form

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,37 @@
-// Assignment code here
+// Assignment code starts here
+function openForm() 
+{
+  document.getElementById("popup").style.display = "block";
+}
 
+function closeForm() 
+{
+  document.getElementById("popup").style.display = "none";
+}
 
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
+function generatePassword()
+{
+  
 
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+
+// Get references to the #generate & #submit element
+//Generate Button now needs to generate pop up form for requirements rather than inital run to writePassword, submit for form now needs to writePassword
+var generateBtn = document.querySelector("#generate");
+var submitBtn = document.querySelector("#submit");
+
+// Write password to the #password input
+function writePassword() 
+{
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+  closeForm();
+
+  passwordText.value = password;
+}
+
+// Add event listener to generate button to open form instead of writePassword
+submitBtn.addEventListener("click", writePassword);
+
+generateBtn.addEventListener("click", openForm);
